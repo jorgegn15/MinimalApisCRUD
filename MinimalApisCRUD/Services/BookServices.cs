51e01b85866eb0bcc7b9657d4cda3e70693ba483
jorgegn15 <jorge323.n@gmail.com>
2024-06-05 08:03:37 -0500

    1 Commit
    1 Commit

---
+++ MinimalApisCRUD/Services/BookServices.cs
@@ -0,0 +1,28 @@
+﻿using MinimalApisCRUD.Context;
+using MinimalApisCRUD.Models;
+using MinimalApisCRUD.Services.Interfaces;
+
+namespace MinimalApisCRUD.Services
+{
+    public class BookServices:IBookServices
+    {
+        private readonly ApiContext _context;
+        public BookServices(ApiContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Book> CrearLibro(BookRequest request)
+        {
+            var book = new Book { 
+                Name = request.Name ?? string.Empty,
+                ISBN = request.Isbn ?? string.Empty,
+            };
+            var createBook = await _context.BookEntity.AddAsync(book);
+
+            await _context.SaveChangesAsync();
+
+            return createBook.Entity;
+        }
+    }
+}
---
+++ MinimalApisCRUD/Services/Interfaces/IBookServices.cs
@@ -0,0 +1,9 @@
+﻿using MinimalApisCRUD.Models;
+
+namespace MinimalApisCRUD.Services.Interfaces
+{
+    public interface IBookServices
+    {
+        Task<Book> CrearLibro(BookRequest request);
+    }
+}
